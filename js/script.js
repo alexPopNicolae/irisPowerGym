411d656256efad4d2507c11020e35265f1ae5062
Alex Poo <alex.pop@nicu.ro>
2016-11-30 19:29:40 +0200

    added the schedul section markup + short jquery

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
-
+    //functia pentru hamburger menu
     $('.hamburger_menu').on('click', function(){
         console.log("Am apasat pe butonul nostru");
+    });
+
+    $('.day').on('click', function(){
+        console.log("Ai apasat pe " + $(this).text());
     });
 
 });
